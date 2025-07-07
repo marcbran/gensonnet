0ecbe9d44e897c495e6e7c9b7956071628c5fd21
Marc Brandenburg <git@marcbrandenburg.net>
2025-07-07 12:47:39 +0200

    feat: jsonnet parse and manifest

---
+++ internal/fun/manifest_jsonnet.go
@@ -0,0 +1,25 @@
+package fun
+
+import (
+	"fmt"
+	"github.com/google/go-jsonnet"
+	"github.com/google/go-jsonnet/ast"
+	intjsonnet "github.com/marcbran/gensonnet/internal/fun/jsonnet"
+)
+
+func ManifestJsonnet() *jsonnet.NativeFunction {
+	return &jsonnet.NativeFunction{
+		Name:   "manifestJsonnet",
+		Params: ast.Identifiers{"jsonnet"},
+		Func: func(input []any) (any, error) {
+			if len(input) != 1 {
+				return nil, fmt.Errorf("jsonnet must be provided")
+			}
+			out, err := intjsonnet.Manifest(input[0])
+			if err != nil {
+				return nil, err
+			}
+			return out, nil
+		},
+	}
+}
---
+++ internal/fun/parse_jsonnet.go
@@ -0,0 +1,29 @@
+package fun
+
+import (
+	"fmt"
+	"github.com/google/go-jsonnet"
+	"github.com/google/go-jsonnet/ast"
+	intjsonnet "github.com/marcbran/gensonnet/internal/fun/jsonnet"
+)
+
+func ParseJsonnet() *jsonnet.NativeFunction {
+	return &jsonnet.NativeFunction{
+		Name:   "parseJsonnet",
+		Params: ast.Identifiers{"jsonnet"},
+		Func: func(input []any) (any, error) {
+			if len(input) != 1 {
+				return nil, fmt.Errorf("jsonnet must be provided")
+			}
+			md, ok := input[0].(string)
+			if !ok {
+				return nil, fmt.Errorf("jsonnet must be a string")
+			}
+			out, err := intjsonnet.Parse(md)
+			if err != nil {
+				return nil, err
+			}
+			return out, nil
+		},
+	}
+}
